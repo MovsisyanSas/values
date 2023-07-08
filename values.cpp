7e6616eba01da32eabecb0883317bf8f5832f121
MovsisyanSas <sasunmov07@gmail.com>
2023-07-08 15:12:14 +0400

    update

--- values.cpp
+++ values.cpp
@@ -1,14 +1,12 @@
 #include <iostream> 
 
 class A {
-	std::string m_String;
 public:
-	A(std::string s) : m_String(s){
-		std::cout << "A par. constructor call " << std::endl;
+	std::string m_String;
+	A(std::string s) : m_String(s) {
 	}
 	A(const A& obj) {
 		this->m_String = obj.m_String;
-		std::cout << "A copy constructor call " << std::endl;
 	}
 	A& operator = (const A& obj) {
 		if (this != &obj)
@@ -17,20 +15,28 @@ public:
 		}
 		return *this;
 	}
-	~A(){
-		std::cout << "A destructor call " << std::endl;
-	}
-public:
-	static void print(const A& obj) { //this function is already usable for lvalue,lvalue reference,const lvalue reference
-		std::cout << obj.m_String << std::endl;
-	}
-	static void print(A&& obj) { // and this one for rvalue reference,const rvalue reference(will work even without const keyword)
-		std::cout << obj.m_String << std::endl;
+	~A() {
 	}
 };
+void print(std::string obj) {
+		std::cout << 'A' << std::endl;
+}
+void print(A& obj) {
+	std::cout << "A&" << std::endl;
+}
+void print(const A& obj) { 
+	std::cout << "const A&" << std::endl;
+}
+void print(A&& obj) { 
+	std::cout << "A&&" << std::endl;
+}
+
 
 int main() {
-	const A str("Say my name!");
-	A::print(str);
-	A::print(A("Haisenberg"));
+	A str("Say my name!");
+	const A str1("Hello");
+	print(str1.m_String);
+	print(str);
+	print(str1);
+	print(A("Haisenberg"));
 }
